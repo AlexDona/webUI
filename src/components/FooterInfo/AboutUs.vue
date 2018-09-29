35b15a423e231f7a5beb32b9d61985202e438593
zxl <991807272@qq.com>
2018-09-29 15:29:43 +0800

    #大小写检测#

--- src/components/FooterInfo/AboutUs.vue
+++ src/components/FooterInfo/AboutUs.vue
@@ -80,7 +80,7 @@ export default {
         height:250px;
         text-align: center;
         width:100%;
-        background: url(../../assets/develop/about-Us.png) no-repeat center center;
+        background: url(../../assets/develop/about-us.png) no-repeat center center;
         -webkit-background-size: 100% 100%;
         background-size: 100% 100%;
         padding-top:100px;
