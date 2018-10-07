9e976ee0d00f58683c4fa3e732b5d9cca8b3d459
yang <13838282547@163.com>
2018-10-07 19:07:56 +0800

    Y

--- src/components/Personal/UserSecuritySettings/UserSecureEmail.vue
+++ src/components/Personal/UserSecuritySettings/UserSecureEmail.vue
@@ -367,9 +367,9 @@ export default {
           margin-left: 55px;
           .send-code-btn {
             width: 90px;
-            height: 36px;
+            /*height: 36px;
             position: absolute;
-            top: -1px;
+            top: -1px;*/
           }
           .input {
             width: 180px;
