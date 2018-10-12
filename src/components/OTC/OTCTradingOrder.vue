37f1fe1e80ce3eedb85cac4dd5c2f5d6b87d59b7
renfuwei <1147907177@qq.com>
2018-10-12 16:03:29 +0800

    ren修复支付宝和微信付款方式账号一样时候选择付款方式账户不变的问题

--- src/components/OTC/OTCTradingOrder.vue
+++ src/components/OTC/OTCTradingOrder.vue
@@ -99,15 +99,28 @@
                       v-model="activePayModeList[index]"
                       @change="changeUserBankInfo(index)"
                     >
-                      <!-- :label="item1.bankType" -->
-                      <!-- :label="$t('m.latestprice')+'(***)'" -->
-                      <!-- :label="$t(`M.${item.i18nName}`)" -->
-                      <!-- :label="language === 'zh_CN'? item1.version : item1.bankType" -->
+                    <!-- 测试 -->
+                    <!-- <el-select
+                      :placeholder="$t('M.otc_MerchantsOrders_chouse') + $t('M.otc_index_Payment_method')"
+                      :no-data-text="$t('M.comm_no_data')"
+                      v-model="activePayModeListID"
+                      @change="changeUserBankInfo(index)"
+                    > -->
+                    <!-- 测试 -->
+                      <!-- 测试 -->
+                      <!-- <el-option
+                        v-for="item1 in item.userBankList"
+                        :key="item1.id"
+                        :label="language === 'zh_CN'? item1.bankName : item1.bankType"
+                        :value="item1.id"
+                      >
+                      </el-option> -->
+                      <!-- 测试 -->
                       <el-option
                         v-for="item1 in item.userBankList"
                         :key="item1.id"
                         :label="language === 'zh_CN'? item1.bankName : item1.bankType"
-                        :value="item1.cardNo"
+                        :value="item1.id"
                       >
                       </el-option>
                     </el-select>
@@ -132,7 +145,7 @@
                     v-if="activeBankType[index] === 'bank'"
                   >
                     <span>
-                      {{$t('M.comm_bill')}}&nbsp;&nbsp;&nbsp;{{$t('M.comm_house')}}: {{activePayModeList[index]}}
+                      {{$t('M.comm_bill')}}&nbsp;&nbsp;&nbsp;{{$t('M.comm_house')}}: {{activedPayAccountArr[index]}}
                     </span>
                   </p>
                   <p
@@ -141,7 +154,7 @@
                   >
                   <!-- 支付宝账户 -->
                     <span>{{$t('M.comm_alipay')}}{{$t('M.comm_bill')}}{{$t('M.comm_house')}}:</span>
-                    <span>{{activePayModeList[index]}}</span>
+                    <span>{{activedPayAccountArr[index]}}</span>
                   </p>
                   <p
                     class="bank-info"
@@ -149,14 +162,14 @@
                   >
                   <!-- 微信账户 -->
                     <span>{{$t('M.comm_weixin')}}{{$t('M.comm_bill')}}{{$t('M.comm_house')}}:</span>
-                    <span>{{activePayModeList[index]}}</span>
+                    <span>{{activedPayAccountArr[index]}}</span>
                   </p>
                   <p
                     class="bank-info"
                     v-if="activeBankType[index] === 'paypal'"
                   >
                     <span>paypal{{$t('M.comm_bill')}}{{$t('M.comm_house')}}:</span>
-                    <span>{{activePayModeList[index]}}</span>
+                    <span>{{activedPayAccountArr[index]}}</span>
                   </p>
                   <p
                     class="bank-info"
@@ -164,7 +177,7 @@
                   >
                   <!-- 西联汇款账户 -->
                     <span>{{$t('M.comm_xilian')}}{{$t('M.comm_bill')}}{{$t('M.comm_house')}}:</span>
-                    <span>{{activePayModeList[index]}}</span>
+                    <span>{{activedPayAccountArr[index]}}</span>
                   </p>
                 </div>
                 <!-- 扫码支付 activeBankCode[index]  :src="item.coinUrl"-->
@@ -723,6 +736,9 @@ export default {
   // props,
   data () {
     return {
+      // 测试
+      activePayModeListID: '', // 选中的支付方式id
+      // 测试
       loading: false,
       // 分页
       currentPage: 1, // 当前页码
@@ -737,8 +753,9 @@ export default {
       tradingOrderList: [],
       // 选中的订单id
       activedTradingOrderId: '',
-      // ren测试支付方式
-      activePayModeList: [], // 当前选中支付方式中的哪一个
+      activedPayAccountArr: [], // 当前选中的订单中付款方式中的付款账号 ：为了解决支付宝和微信账号一样做的bug修复
+      // 支付方式
+      activePayModeList: [], // 当前选中支付方式中的哪一个 -->为了解决支付宝和微信账号一样做的bug修复// 当年选中的支付方式的id
       activeBankFidList: [], // 当前选中支付方式的id
       activeBankProv: [], // 当前选中支付银行所在省
       activeBankCity: [], // 当前选中支付银行所在市
@@ -892,17 +909,26 @@ export default {
     },
     // 3.0 改变交易方式
     changeUserBankInfo (index) {
-      console.log(index)
-      console.log('选中订单的订单号')
+      // console.log(this.activePayModeList[index]) // 就回退到这吧
+      console.log('第' + index + '条数据')
+      console.log('选中的订单id')
       console.log(this.tradingOrderList[index].id)
       this.activedTradingOrderId = this.tradingOrderList[index].id
       this.tradingOrderList[index].userBankList.forEach((item) => {
-        if (item.cardNo == this.activePayModeList[index]) {
+        if (item.id == this.activePayModeList[index]) {
+        // 测试
+        // if (item.id == this.activePayModeListID) {
+        // 测试
+          this.activedPayAccountArr[index] = item.cardNo
+          console.log('选中的付款账号：' + this.activedPayAccountArr[index])
+          // 测试
+          // this.activePayModeList[index] = item.cardNo
+          // 测试
           this.activeBankFidList[index] = item.id
           console.log('选中的支付方式id')
           console.log(this.activeBankFidList[index])
-          console.log('账号')
-          console.log(this.activePayModeList[index])
+          // console.log('账号')
+          // console.log(this.activePayModeList[index])
           this.activitedPayStyleId = this.activeBankFidList[index]
           // 省
           this.activeBankProv[index] = item.prov
@@ -916,6 +942,7 @@ export default {
           this.activeBankDetailAddress[index] = item.address
           // 支付类型
           this.activeBankType[index] = item.bankType
+          console.log('支付类型')
           console.log(this.activeBankType[index])
           // 支付码
           this.activeBankCode[index] = item.qrcode
