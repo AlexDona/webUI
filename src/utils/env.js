2200b1099b31c9ba9f0bb98f97253ede3bb59cca
zxl <991807272@qq.com>
2018-11-01 18:04:07 +0800

    #api返回#

--- src/utils/env.js
+++ src/utils/env.js
@@ -15,9 +15,7 @@ domain = domain.join('/')
 if (!domain.endsWith('#')) {
   domain += '#'
 }
-let xDomain = window.location.host.split(':')[0]
-xDomain = xDomain.startsWith('www') ? xDomain.slice(4) : xDomain
-
+console.log(domain)
 let routerMode = 'hash'
 if (process.env.NODE_ENV == 'development') {
   /* apiCommonUrl --------------------------------------------- */
@@ -55,12 +53,12 @@ if (process.env.NODE_ENV == 'development') {
   // apiCommonUrl = 'https://rest.fubt.top/'
   // apiCommonUrl = 'http://192.168.1.200:8888/' // 本地测试
   // apiCommonUrl = 'http://api.new.fubt.com/' // 本地测试
-  apiCommonUrl = `https://api.${xDomain}/` // 内部测试
+  apiCommonUrl = 'https://api.new.bzu.com/' // 内部测试
 
   /* socketUrl --------------------------------------------- */
 
   socketUrl = 'wss://ws.bzu.com/market'
-  loginSocketUrl = `ws://api.${xDomain}/qrcodeLogin/`
+  loginSocketUrl = 'ws://api.new.bzu.com/qrcodeLogin/'
   // socketUrl = 'ws://192.168.1.200:8087/market'
 
   /* domain --------------------------------------------- */
