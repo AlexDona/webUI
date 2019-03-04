395ac72e3f38292295d31706d87f95269cd49a82
任付伟 <1147907177@qq.com>
2019-03-04 18:20:05 +0800

    style: 订单状态表头样式修复，接口loading改写未完

--- src/components/OTC/OTCFreezingOrder.vue
+++ src/components/OTC/OTCFreezingOrder.vue
@@ -341,6 +341,7 @@ export default {
           border: 1px solid #485776;
           color: #a9bed4;
           background-color: $mainContentNightBgColor;
+          box-shadow: -2px 3px 5px 1px #191e28;
         }
 
         > .freezing-table-body {
