7cfed0e7a0e56ba46e86bd426ecad86f9d770908
wt <1256625313@qq.com>
2019-07-22 15:54:52 +0800

    feature: 修改按钮白色主题背景颜色

--- src/components/Personal/AccountBalance/BillingDetails.vue
+++ src/components/Personal/AccountBalance/BillingDetails.vue
@@ -854,7 +854,7 @@ export default {
 
           > .search-button {
             color: #fff;
-            background: linear-gradient(9deg, rgba(43, 57, 110, 1), rgba(42, 80, 130, 1));
+            background: linear-gradient(90deg, rgba(106, 182, 244, 1) 0%, rgba(49, 135, 218, 1) 100%);
           }
 
           .demonstration {
