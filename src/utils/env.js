320b91a420c489b17afc61c18d22c8545eca1d40
赵鑫磊 <991807272.com>
2019-07-22 14:11:22 +0800

    style: 恢复测试地址

--- src/utils/env.js
+++ src/utils/env.js
@@ -46,9 +46,10 @@ const devTestConfig = {
   // socketUrl: 'ws://192.168.2.200:8087/market',
 
   // HuaHaoTian
-  // socketUrl: 'ws://192.168.0.52:8087/market',
-  // loginSocketUrl: 'ws://192.168.2.200:8888/qrcodeLogin/',
-  // OTCIMSocketUrl: 'ws://192.168.2.210:8888/websoc'
+  socketUrl: 'ws://192.168.2.200:8087/market',
+  apiCommonUrl: 'http://192.168.2.200:8888/',
+  loginSocketUrl: 'ws://192.168.2.200:8888/qrcodeLogin/',
+  OTCIMSocketUrl: 'ws://192.168.2.200:8888/websoc'
 }
 
 const dev210Config = {
@@ -75,12 +76,12 @@ const newProdConfig = {
 switch (process.env.NODE_ENV) {
   case 'development':
     // 本地开发
-    // targetConfig = {...targetConfig, ...devTestConfig, xDomain: 'new.test.com'}
+    targetConfig = {...targetConfig, ...devTestConfig, xDomain: 'new.test.com'}
     // targetConfig = {...targetConfig, ...devTestConfig, xDomain: 'me.com'}
     // 210开发环境
     // targetConfig = {...dev210Config, xDomain: 'me.com'}
     // 测试环境 （外网）
-    targetConfig = {...prodConfig, xDomain: 'new.bzu.com'}
+    // targetConfig = {...prodConfig, xDomain: 'new.bzu.com'}
     // 生产环境
     // targetConfig = {...prodConfig, xDomain: 'fubt.co'}
     // targetConfig = {...newProdConfig, xDomain: 'coin67.cn'}
