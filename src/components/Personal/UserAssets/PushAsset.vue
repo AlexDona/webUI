080090c17746e6226680aef8f60c1635ad057324
wt <1256625313@qq.com>
2019-07-23 10:37:54 +0800

    feature: push资产按钮颜色修改

--- src/components/Personal/UserAssets/PushAsset.vue
+++ src/components/Personal/UserAssets/PushAsset.vue
@@ -1137,7 +1137,7 @@ export default {
 
             .form-button-common {
               color: #fff;
-              background: linear-gradient(0deg, rgba(43, 57, 110, 1), rgba(42, 80, 130, 1));
+              background: linear-gradient(90deg, rgba(18, 71, 133, 1) 0%, rgba(42, 59, 97, 1) 100%);
             }
           }
         }
@@ -1298,7 +1298,7 @@ export default {
 
             .form-button-common {
               color: #fff;
-              background: linear-gradient(0deg, rgba(43, 57, 110, 1), rgba(42, 80, 130, 1));
+              background: linear-gradient(90deg, rgba(106, 182, 244, 1) 0%, rgba(49, 135, 218, 1) 100%);
             }
           }
         }
