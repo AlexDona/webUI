bcd1963bb44bf444baff50590c19a0ab82ae4e40
wt <1256625313@qq.com>
2019-08-01 14:56:44 +0800

    style: 修改按钮字体颜色

--- src/components/Personal/AccountBalance/AccountAssets.vue
+++ src/components/Personal/AccountBalance/AccountAssets.vue
@@ -2072,7 +2072,7 @@ export default {
       }
 
       .button-color {
-        color: rgba(255, 255, 255, .7);
+        color: rgba(255, 255, 255, 1);
         background: linear-gradient(81deg, rgba(43, 57, 110, 1) 0%, rgba(42, 80, 130, 1) 100%);
       }
 
