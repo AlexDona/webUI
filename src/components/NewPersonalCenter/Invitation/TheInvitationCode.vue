eecd28b358fb8a8843a43545c6863016929dbad8
赵鑫磊 <991807272.com>
2019-08-12 15:10:50 +0800

    style: jquery 移除

--- src/components/NewPersonalCenter/Invitation/TheInvitationCode.vue
+++ src/components/NewPersonalCenter/Invitation/TheInvitationCode.vue
@@ -228,7 +228,7 @@ export default {
     }),
     inviteUrl () {
       // console.log(domain)
-      return `${domain}/${this.$routes_X.register}?showId=${this.$userInfo_X.showId}`
+      return `${domain}/${this.$routes_X.login}/${this.$routes_X.register}/${this.$userInfo_X.showId}`
     },
     hasInviter () {
       return this.$userInfo_X.inviter
