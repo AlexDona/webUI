5c3f86c85389f054b15003a4713b517513b01bba
赵鑫磊 <991807272.com>
2019-08-13 10:56:03 +0800

    style: 更新代码

---
+++ build/uploadI18nShell.js
@@ -0,0 +1,9 @@
+/**
+ * author: zhaoxinlei
+ * create: 20190813
+ * description: 当前文件为 执行 更新 dist 文件夹的 shell 脚本 文件
+ */
+
+const exec = require('child_process').execSync
+
+exec('bash ../dist/uploadProjectToTest.sh')
