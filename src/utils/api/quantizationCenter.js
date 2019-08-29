bdfea1fc5d8a69144a6dbabceac7d9017121616b
wt <1256625313@qq.com>
2019-08-29 11:20:56 +0800

    feature: 量化api

--- src/utils/api/quantizationCenter.js
+++ src/utils/api/quantizationCenter.js
@@ -1,4 +1,6 @@
 import {get} from './axios'
 import {handleRequest} from '../commonFunc'
 // 获取策略列表
-export const getStrategyList = () => handleRequest(() => get('/strategyConfig/list'))
+export const getStrategyList = params => handleRequest(() => get('/strategyConfig/list', params))
+// 获取我的策略列表
+export const getMyStrategyList = params => handleRequest(() => get('/strategyUser/ownStrategy', params))
