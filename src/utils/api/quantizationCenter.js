1a525acf1a5c145ec964155475ceff1772329e19
wt <1256625313@qq.com>
2019-08-30 17:25:57 +0800

    feature: 量化api更新

--- src/utils/api/quantizationCenter.js
+++ src/utils/api/quantizationCenter.js
@@ -4,3 +4,7 @@ import {handleRequest} from '../commonFunc'
 export const getStrategyList = params => handleRequest(() => get('/strategyConfig/list', params))
 // 获取我的策略列表
 export const getMyStrategyList = params => handleRequest(() => get('/strategyUser/ownStrategy', params))
+// 立即购买
+export const getBuyDialogList = params => handleRequest(() => get('/strategyConfig/getStrategyConfigById', params))
+// 购买策略
+export const buyStrategy = params => handleRequest(() => get('/strategyUser/buyStrategy', params))
