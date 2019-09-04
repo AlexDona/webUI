95a01a45f97f004f9c26e6e7f3c14e5e0d5a08af
wt <1256625313@qq.com>
2019-09-04 18:34:12 +0800

    feature: 设置查看策略缓存

--- src/vuex/Quantization/mutations.js
+++ src/vuex/Quantization/mutations.js
@@ -6,5 +6,7 @@ export default {
   },
   [SET_SEARCH_STRATEGY_DATA] (state, data) {
     state.searchData = _.get(data, 'data')
+    // 设置查看策略请求的数据缓存
+    sessionStorage.setItem('SEARCH_STRATEGY_DATA', JSON.stringify(state.searchData, null, '\t'))
   }
 }
